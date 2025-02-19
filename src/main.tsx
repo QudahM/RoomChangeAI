414e6a6e1f5782098bef2a74bde9e369dced29ab
Mohammad Qudah <qudahmohammad2002@gmail.com>
2025-02-19 04:04:16 -0500

    fixing main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const basename = import.meta.env.BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename={basename}>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
