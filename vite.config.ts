1d30a274391d7b02267b2c9ce421e3b7ae926008
Mohammad Qudah <qudahmohammad2002@gmail.com>
2025-02-23 18:11:53 -0500

    updating base in vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ if (process.env.TEMPO === "true") {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./", 
+  base: "/RoomChangeAI/", 
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
